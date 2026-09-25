6377bed8a983a2ff29f02e796ef3c3a7f28bf9fb
Ibrahim Khan <shiraishi93@gmail.com>
2026-09-25 17:03:11 +0200

    submit new task to server, but no server yet

--- webdev/src/main.jsx
+++ webdev/src/main.jsx
@@ -30,8 +30,16 @@ function NewTaskForm({onNewTask}) {
 function TaskApplication() {
     const [tasks, setTasks] = useState([])
 
-    function handleNewTask(task) {
+    async function handleNewTask(task) {
         setTasks((prevTasks) => [...prevTasks, task]);
+
+        await fetch("/api/tasks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(task),
+        })
     }
 
     return (
